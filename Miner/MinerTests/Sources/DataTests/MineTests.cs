6656d5be437669a4e3987708f6a3bfac828924e6
denis96z <zinoviev.denis@yandex.ru>
2017-12-04 04:27:06 +0300

    100% Data domain tests coverage.

---
+++ Miner/MinerTests/Sources/DataTests/MineTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+using Miner.Data;
+
+namespace MinerTests.DataTests
+{
+    [TestFixture]
+    public class MineTests
+    {
+        [Test]
+        public void TestConstructor_ObjectIsCreated()
+        {
+            var mine = new Mine();
+        }
+    }
+}
---
+++ Miner/MinerTests/Sources/DataTests/NumberOfMinesTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+using Miner.Data;
+
+namespace MinerTests.DataTests
+{
+    [TestFixture]
+    public class NumberOfMinesTests
+    {
+        [Test]
+        public void TestConstructor_ValidValue_ObjectIsCreated()
+        {
+            for (int i = NumberOfMines.MIN_VALUE; i <= NumberOfMines.MAX_VALUE; i++)
+            {
+                var numberOfMines = new NumberOfMines(i);
+            }
+        }
+
+        [TestCase(NumberOfMines.MIN_VALUE - 1)]
+        [TestCase(NumberOfMines.MAX_VALUE + 1)]
+        public void TestConstructor_InvalidValue_ThrowsException(int value)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var numberOfMines = new NumberOfMines(value);
+            });
+        }
+
+        [Test]
+        public void TestValueGetterSetter_ValidValue()
+        {
+            var numberOfMines = new NumberOfMines();
+            for (int i = NumberOfMines.MIN_VALUE; i <= NumberOfMines.MAX_VALUE; i++)
+            {
+                numberOfMines.Value = i;
+                Assert.AreEqual(i, numberOfMines.Value);
+            }
+        }
+
+        [TestCase(NumberOfMines.MIN_VALUE - 1)]
+        [TestCase(NumberOfMines.MAX_VALUE + 1)]
+        public void TestValueGetterSetter_InvalidValue_ThrowsException(int value)
+        {
+            var numberOfMines = new NumberOfMines();
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                numberOfMines.Value = value;
+            });
+        }
+    }
+}
